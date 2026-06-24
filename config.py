985e9893e03ccef1fd5c576143963a1286f652af
ksm30289 <ksm30289@gmail.com>
2026-06-24 16:52:42 +0900

    Update config.py

--- config.py
+++ config.py
@@ -16,8 +16,7 @@ GOOGLE_SERVICE_ACCOUNT_JSON = required_env("GOOGLE_SERVICE_ACCOUNT_JSON")
 GOOGLE_CREDENTIALS = json.loads(GOOGLE_SERVICE_ACCOUNT_JSON)
 
 DISCORD_SHEET = os.getenv("DISCORD_SHEET", "디스코드 동향")
-NEGATIVE_SHEET = os.getenv("NEGATIVE_SHEET", "언디셈버_KR_부정 동향")
-FLOOR_SHEET = os.getenv("FLOOR_SHEET", "언디셈버_KR_플로어 동향")
+COMMUNITY_SHEET = "언디셈버_KR_동향"
 SUMMARY_SHEET = os.getenv("SUMMARY_SHEET", "일별 동향 요약")
 
 OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
